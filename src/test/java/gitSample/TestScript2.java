dc03da49acd7e315b74ff54c0f4f57ca6026820f
Manu <Manu@Manu>
2022-08-24 13:06:18 +0530

    third changes

---
+++ src/test/java/gitSample/TestScript2.java
@@ -0,0 +1,13 @@
+package gitSample;
+
+import org.testng.annotations.Test;
+
+public class TestScript2 {
+	
+	@Test
+	public void testScriptmethod()
+	{
+		System.out.println(" hii it manu ");
+	}
+
+}
